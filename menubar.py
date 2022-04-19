c5d8a6f768abd446198e42a789bfc60e1f969715
Maharshi Sinha <72745563+maharshi-sinha@users.noreply.github.com>
2022-04-19 19:04:05 +0530

    Create menubar.py

---
+++ menubar.py
@@ -0,0 +1,24 @@
+
+# importing messagebox class 
+from tkinter.messagebox import *
+  
+# Showing various messages 
+  
+print(askokcancel("askokcancel", "Ok or Cancel")) 
+  
+print(askquestion("askquestion", "Question?")) 
+  
+print(askretrycancel("askretrycancel", "Retry or Cancel")) 
+  
+print(askyesno("askyesno", "Yes or No")) 
+  
+print(askyesnocancel("askyesnocancel", "Yes or No or Cancel")) 
+  
+print(showerror("showerror", "Error")) 
+  
+print(showinfo("showinfo", "Information")) 
+  
+print(showwarning("showwarning", "Warning")) 
+  
+# print statement is used so that we can, 
+# print the returned value by the function 
